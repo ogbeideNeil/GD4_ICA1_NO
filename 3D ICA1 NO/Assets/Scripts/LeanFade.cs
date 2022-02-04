6da0853bc83c0164cc8d408def90ee48bcb1d9f0
Neil Ogbeide <d00205601@student.dkit.ie>
2022-02-04 20:21:37 +0000

    Fixing Commit Issues

--- 3D ICA1 NO/Assets/Scripts/LeanFade.cs
+++ 3D ICA1 NO/Assets/Scripts/LeanFade.cs
@@ -17,11 +17,22 @@ public class LeanFade : MonoBehaviour
     [SerializeField]
     private LeanTweenType easeType = LeanTweenType.linear;
 
+    private Vector3 startpos;
+
+    private Vector3 min;// = 0,0,0;
+
     public void OnTriggerEnter(Collider collider)
     {
+        startpos = objectToFade.transform.position;
        
             //fade to transparent using the ease curve
             LeanTween.alpha(objectToFade, 0.3f, fadeTimeSecs);//.setEase(easeType);
+        if(objectToFade.name == "Cube (6)" || objectToFade.name == "wall+door (1)")
+        {
+            //LeanTween.alpha(objectToFade, 0f, fadeTimeSecs);
+            //LeanTween.moveY(objectToFade, 50f, fadeTimeSecs);
+            //LeanTween.scale(objectToFade)
+        }
         
     }
 
@@ -30,7 +41,14 @@ public class LeanFade : MonoBehaviour
         
             //fade to opaque using the ease curve
             LeanTween.alpha(objectToFade, 1, fadeTimeSecs).setEase(easeType);
-        
+
+        if (objectToFade.name == "Cube (6)" || objectToFade.name == "wall+door (1)")
+        {
+            //LeanTween.alpha(objectToFade, 0f, fadeTimeSecs);
+            //LeanTween.moveY(objectToFade, 0f, fadeTimeSecs);
+            //LeanTween.scale(objectToFade)
+        }
+
     }
 
 
